07afa270a83b3acf35309c8b028f66509c737bef
胡兵伟 <bingwe.hu@icloud.com>
2020-06-23 00:38:07 +0800

    update

--- communication/js/content.js
+++ communication/js/content.js
@@ -5,13 +5,16 @@ console.log('content.js 已注入')
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log('收到来自 ' + (sender.tab ? "content-script(" + sender.tab.url + ")" : "popup或者background") + ' 的消息：', request);
 
+  setTimeout(() => {
+    sendMessageToPopup();
+  }, 2000);
+
   sendResponse('我收到你的消息了：' + JSON.stringify(request));
 });
 
-// 主动发送消息给后台
-// 要演示此功能，请打开控制台主动执行sendMessageToBackground()
-function sendMessageToBackground(message) {
+// 主动发送消息给popup
+function sendMessageToPopup(message) {
 	chrome.runtime.sendMessage({greeting: message || '你好，我是content-script呀，我主动发消息给后台！'}, function(response) {
-		tip('收到来自后台的回复：' + response);
+		console.log('收到来自后台的回复：' + response);
 	});
 }
